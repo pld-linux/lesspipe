cf7a2592dcc4f9d0ab150018593c5b163af62d84
kloczek <kloczek@pld-linux.org>
2001-06-26 09:15:04 +0000

    - patch ditrectly lesspipe.sh script.
    Changed files:
        lesspipe.sh -> 1.2

--- lesspipe.sh
+++ lesspipe.sh
@@ -25,31 +25,25 @@
 # variable is set:   LESSOPEN="|lesspipe.sh %s"
 
 lesspipe() {
-  case "$1" in
-  *.tar) tar tvvf $1 2>/dev/null ;; # View contents of .tar and .tgz files
-  *.tgz) tar tzvvf $1 2>/dev/null ;;
-  *.tar.gz) tar tzvvf $1 2>/dev/null ;;
-  *.tar.Z) tar tzvvf $1 2>/dev/null ;;
-  *.tar.z) tar tzvvf $1 2>/dev/null ;;
-  *.tar.bz2) tar tyvvf $1 2>/dev/null ;;
-  *.Z) gzip -dc $1  2>/dev/null ;; # View compressed files correctly
-  *.z) gzip -dc $1  2>/dev/null ;;
-  *.gz) gzip -dc $1  2>/dev/null ;;
-  *.bz2) bzip2 -dc $1 2>/dev/null ;;
-  *.zip) unzip -l $1 2>/dev/null ;;
-  *.1|*.2|*.3|*.4|*.5|*.6|*.7|*.8|*.9|*.n|*.man) FILE=`file -L $1` ; # groff src
-    FILE=`echo $FILE | cut -d ' ' -f 2`
-    if [ "$FILE" = "troff" ]; then
-      groff -s -p -t -e -Tascii -mandoc $1
-    fi ;;
-#  *) FILE=`file -L $1` ; # Check to see if binary, if so -- view with 'strings'
-#    FILE1=`echo $FILE | cut -d ' ' -f 2`
-#    FILE2=`echo $FILE | cut -d ' ' -f 3`
-#    if [ "$FILE1" = "Linux/i386" -o "$FILE2" = "Linux/i386" \
-#         -o "$FILE1" = "ELF" -o "$FILE2" = "ELF" ]; then
-#      strings $1
-#    fi ;;
+	case ""$1"" in
+	*.tar) tar tvvf "$1" 2>/dev/null ;; # View contents of .tar and .tgz files
+	*.tgz) tar tzvvf "$1" 2>/dev/null ;;
+	*.tar.gz) tar tzvvf "$1" 2>/dev/null ;;
+	*.tar.Z) tar tzvvf "$1" 2>/dev/null ;;
+	*.tar.z) tar tzvvf "$1" 2>/dev/null ;;
+	*.tar.bz2) tar tyvvf "$1" 2>/dev/null ;;
+	*.Z) gzip -dc "$1"  2>/dev/null ;; # View compressed files correctly
+	*.z) gzip -dc "$1"  2>/dev/null ;;
+	*.gz) gzip -dc "$1"  2>/dev/null ;;
+	*.bz2) bzip2 -dc "$1" 2>/dev/null ;;
+	*.zip) unzip -l "$1" 2>/dev/null ;;
+	*.1|*.2|*.3|*.4|*.5|*.6|*.7|*.8|*.9|*.l|*.n|*.man) FILE=`file -L "$1"` ; # groff src
+		FILE=`echo $FILE | cut -d ' ' -f 2`
+		if [ "$FILE" = "troff" ]; then
+			groff -s -p -t -e -Tlatin1 -mandoc "$1"
+		fi ;;
+	*) FILE=`file -L "$1"` ; # Check to see if binary, if so -- view with 'strings'
   esac
 }
 
-lesspipe $1
+lesspipe "$1"
