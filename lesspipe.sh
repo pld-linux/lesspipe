28591cbf4127802e2d8cc3f535c31999e7cf1cd4
Elan Ruusamäe <glen@pld-linux.org>
2005-01-13 19:24:47 +0000

    - simplified
    Changed files:
        lesspipe.sh -> 1.7

--- lesspipe.sh
+++ lesspipe.sh
@@ -25,19 +25,19 @@
 # variable is set:   LESSOPEN="|lesspipe.sh %s"
 
 lesspipe() {
-	case ""$1"" in
-	*.tar) tar tvvf "$1" 2>/dev/null ;; # View contents of .tar and .tgz files
-	*.tgz) tar tzvvf "$1" 2>/dev/null ;;
-	*.tar.gz) tar tzvvf "$1" 2>/dev/null ;;
-	*.tar.Z) tar tzvvf "$1" 2>/dev/null ;;
-	*.tar.z) tar tzvvf "$1" 2>/dev/null ;;
-	*.tar.bz2) bzcat "$1" | tar tvvf - 2>/dev/null ;;
-	*.Z) gzip -dc "$1"  2>/dev/null ;; # View compressed files correctly
-	*.z) gzip -dc "$1"  2>/dev/null ;;
-	*.gz) gzip -dc "$1"  2>/dev/null ;;
-	*.bz) bzip -dc "$1" 2>/dev/null ;;
-	*.bz2) bzip2 -dc "$1" 2>/dev/null ;;
-	*.zip) unzip -l "$1" 2>/dev/null ;;
+	case "$1" in
+	*.tar) tar tvvf "$1" ;; # View contents of .tar and .tgz files
+	*.tgz) tar tzvvf "$1" ;;
+	*.tar.gz) tar tzvvf "$1" ;;
+	*.tar.Z) tar tzvvf "$1" ;;
+	*.tar.z) tar tzvvf "$1" ;;
+	*.tar.bz2) bzcat "$1" | tar tvvf - ;;
+	*.Z) gzip -dc "$1" ;; # View compressed files correctly
+	*.z) gzip -dc "$1" ;;
+	*.gz) gzip -dc "$1" ;;
+	*.bz) bzip -dc "$1" ;;
+	*.bz2) bzip2 -dc "$1" ;;
+	*.zip) unzip -l "$1" ;;
 	*.1|*.2|*.3|*.4|*.5|*.6|*.7|*.8|*.9|*.l|*.n|*.man) FILE=`file -L "$1"` ; # groff src
 		FILE=`echo $FILE | cut -d ' ' -f 2`
 		if [ "$FILE" = "troff" ]; then
@@ -47,4 +47,4 @@ lesspipe() {
   esac
 }
 
-lesspipe "$1"
+lesspipe "$1" 2>/dev/null
