825582905f6ac2ca261b54cab187a7943c9f76e0
Elan Ruusamäe <glen@pld-linux.org>
2005-01-13 19:28:56 +0000

    - merge entries, add .rpm and .cpio
    Changed files:
        lesspipe.sh -> 1.8

--- lesspipe.sh
+++ lesspipe.sh
@@ -26,18 +26,15 @@
 
 lesspipe() {
 	case "$1" in
-	*.tar) tar tvvf "$1" ;; # View contents of .tar and .tgz files
-	*.tgz) tar tzvvf "$1" ;;
-	*.tar.gz) tar tzvvf "$1" ;;
-	*.tar.Z) tar tzvvf "$1" ;;
-	*.tar.z) tar tzvvf "$1" ;;
+	*.tar) tar tvvf "$1" ;;
+	*.tgz|*.tar.gz|*.tar.[Zz]) tar tzvvf "$1" ;;
 	*.tar.bz2) bzcat "$1" | tar tvvf - ;;
-	*.Z) gzip -dc "$1" ;; # View compressed files correctly
-	*.z) gzip -dc "$1" ;;
-	*.gz) gzip -dc "$1" ;;
+	*.[Zz]|*.gz) gzip -dc "$1" ;;
 	*.bz) bzip -dc "$1" ;;
 	*.bz2) bzip2 -dc "$1" ;;
 	*.zip) unzip -l "$1" ;;
+	*.rpm) rpm -qpivl --changelog -- "$1" ;;
+	*.cpi|*.cpio) cpio -itv < "$1" ;;
 	*.1|*.2|*.3|*.4|*.5|*.6|*.7|*.8|*.9|*.l|*.n|*.man) FILE=`file -L "$1"` ; # groff src
 		FILE=`echo $FILE | cut -d ' ' -f 2`
 		if [ "$FILE" = "troff" ]; then
